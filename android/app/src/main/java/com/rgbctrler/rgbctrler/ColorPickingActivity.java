11347099d533f74fe96a8125620b71cdf5d6c72c
koincidencia <kazykazy91@gmail.com>
2017-06-11 11:03:06 +0200

    add send hostname

--- android/app/src/main/java/com/rgbctrler/rgbctrler/ColorPickingActivity.java
+++ android/app/src/main/java/com/rgbctrler/rgbctrler/ColorPickingActivity.java
@@ -1,11 +1,16 @@
 package com.rgbctrler.rgbctrler;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.chiralcode.colorpicker.ColorPicker;
@@ -39,5 +44,45 @@ public class ColorPickingActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.rgbctrler_options, menu);
         return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.opt_dev_name:
+                setDeviceNameDialog();
+                return true;
+            case R.id.opt_wifi_sta:
+                return true;
+            case R.id.opt_wifi_ap:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    protected void setDeviceNameDialog() {
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Device name setup");
+        builder.setMessage("Alert message to be shown");
+        builder.setView(input);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String devName =  input.getText().toString();
+                rgbCtrler.SendHostname(devName, getApplicationContext());
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
     }
 }
